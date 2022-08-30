0d6191f36157b2527dce5d663c157f2a5f64ab12
ericbarefoot <eabarefoot@gmail.com>
2022-08-30 20:53:15 +0000

    Deploying to gh-pages from  @ 008b6f55d5147f9b1622829f5ed787a53d2e9ff9 🚀

--- examples/variable_velocity-4.py
+++
@@ -1,5 +0,0 @@
-# we create the model here, just to be sure it works (for good docs)
-with pyDeltaRCM.shared_tools._docs_temp_directory() as output_dir:
-    mdl = ChangingVelocityModel(
-        end_time=end_time,
-        out_dir=output_dir)
